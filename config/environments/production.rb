010b61c9c7faddbac502cba49b44f695ffa0456d
nnittop909 <tophie.pumihic@gmail.com>
2022-09-21 15:37:27 +0800

    Fix styles to follow bootstrap and remove sb_admin custom css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_view.preload_links_header = false
 end
