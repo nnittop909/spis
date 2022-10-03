7ba48e1b6894b1f3a36b84e22f7b4abb92e0f980
nnittop909 <tophie.pumihic@gmail.com>
2022-10-03 13:54:01 +0800

    Fix parsing of filename

--- app/services/filename_parser.rb
+++ app/services/filename_parser.rb
@@ -1,8 +1,9 @@
 class FilenameParser
 
-	attr_reader :filename
+	attr_reader :filename, :documentable
 	def initialize(args={})
     @filename = args[:filename]
+    @documentable = args[:documentable]
 	end
 
 	def parse!
@@ -16,10 +17,18 @@ class FilenameParser
 
   def post_array #remove letters
     array = [filename_array[0], filename_array[1]].join("-").split("-")
-    [array[0].gsub(/[^0-9,.]/, ""), "#{array[1].gsub(/[^0-9,.]/, "")}R"]
+    [array[0].gsub(/[^0-9,.]/, ""), second_value(array)]
   end
 
   def filename_array #remove "PO"
     @filename.split("-").to_a.map{|a| a.include?("PO") ? nil : [a]}.compact
+  end
+
+  def second_value(array)
+    if @filename.include?("R")
+      "#{array[1].gsub(/[^0-9,.]/, "")}R"
+    else
+      array[1].gsub(/[^0-9,.]/, "")
+    end
   end
 end
