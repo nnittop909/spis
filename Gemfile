0c946be3088f052f5bb6bd388c655595f2c9c3ac
Christopher Pumihic <tophie.pumihic@gmail.com>
2022-11-25 10:21:21 +0800

    Restrict settings page to regular user.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
-gem "rails", "~> 7.0.2", ">= 7.0.2.4"
+gem "rails", "~> 7.0.4"
 gem "sassc-rails"
 gem "jquery-rails"
 gem 'jquery-ui-rails'
--- test/models/term_test.rb
+++ test/models/term_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
-class TermTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+describe Term do
+  describe 'associations' do
+    it { is_expected.to belong_to :termable }
+    it { is_expected.to belong_to :political_party }
+    it { is_expected.to belong_to :position }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :start_of_term }
+    it { is_expected.to validate_presence_of :end_of_term }
+    it { is_expected.to validate_presence_of :position }
+    it { is_expected.to validate_presence_of :appointment_status }
+  end
