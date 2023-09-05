89c71fb41f83ca5863c51662605e4ebb6522a33a
Christopher Pumihic <tophie.pumihic@gmail.com>
2023-09-05 15:43:58 +0800

    Add date and category filter in exporting records of resolutions and ordinances.

--- app/controllers/settings/exports/ordinances_controller.rb
+++ app/controllers/settings/exports/ordinances_controller.rb
@@ -3,7 +3,25 @@ module Settings
 		class OrdinancesController < ApplicationController
 
 			def index
-				@ordinances = Ordinance.all.sort_by(&:parsed_number).reverse
+				@from_date = params[:from_date].present? ? params[:from_date] : 999.years.ago
+				@to_date = params[:to_date].present? ? params[:to_date] : Date.today
+				@category = params[:category_id].present? ? Category.find(params[:category_id]) : []
+
+				if params[:author_id].present?
+					@author = Member.find(params[:author_id])
+					@ordinances = Ordinances::AuthoredOrdinanceFinder.new(
+						author: @author, category: @category, from_date: @from_date, to_date: @to_date
+					).query!
+				elsif params[:sponsor_id].present?
+					@sponsor = Committee.find(params[:sponsor_id])
+					@ordinances = Ordinances::SponsoredOrdinanceFinder.new(
+						sponsor: @sponsor, category: @category, from_date: @from_date, to_date: @to_date
+					).query!
+				else
+					@ordinances = Ordinances::DefaultOrdinanceFinder.new(
+						from_date: @from_date, to_date: @to_date, category: @category
+					).query!
+				end
 			  respond_to do |format|
 		      format.xlsx { render xlsx: "index", disposition: 'inline', filename: "Ordinances.xlsx" }
 		    end
